5ba65fa68782fa8d5a85f4335a9183a985a2337b
bishnuchhantyal <47364252+bishnuchhantyal@users.noreply.github.com>
2020-04-15 01:26:04 -0500

    Update CategoryRepository.java
    Updated repository

--- src/main/java/com/uwm/onlineshopping/repository/CategoryRepository.java
+++ src/main/java/com/uwm/onlineshopping/repository/CategoryRepository.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Repository;
 
 import com.uwm.onlineshopping.model.CategoryEntity;
 
+// This classs extends JpaReposigory
+// helps to support crude function 
+
 @Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
 
